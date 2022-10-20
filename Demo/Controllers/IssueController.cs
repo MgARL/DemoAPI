ab4a2a7a9c9557058b2fe0b2a0012bc530d24d62
Miguel <Miguel@DESKTOP-TT2C836>
2022-10-19 21:59:10 -0400

    -Created basic controllers - Configured Database

---
+++ Demo/Controllers/IssueController.cs
@@ -0,0 +1,38 @@
+﻿using Demo.Data;
+using Demo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDBContext _dbContext;
+        public IssueController(IssueDBContext context) => _dbContext = context;
+
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> GetIssue()
+        {
+            return await _dbContext.Issues.ToListAsync();
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var issue = await _dbContext.Issues.FindAsync(id);
+            return issue == null ? NotFound() : Ok(issue);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Issue issue)
+        {
+            await _dbContext.Issues.AddAsync(issue);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new {id = issue.Id}, issue);
+        }
+    }
+}
---
+++ Demo/Data/IssueDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Data
+{
+    public class IssueDBContext : DbContext
+    {
+        public IssueDBContext(DbContextOptions<IssueDBContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
